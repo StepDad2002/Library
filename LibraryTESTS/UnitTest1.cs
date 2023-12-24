ba4676a2703c81f1047d7ebdd29eee8fee844776
StepDad2002 <chermishan@gmail.com>
2023-12-24 10:47:09 +0200

    few files for tests

---
+++ LibraryTESTS/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace LibraryTESTS;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        //Arrange
+        //adadadadadadada
+        //Act
+        
+        //Assert
+    }
+}
